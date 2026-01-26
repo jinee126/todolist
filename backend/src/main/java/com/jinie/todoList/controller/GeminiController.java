75cea7349305b08b507a6f458cec4b7743f0f761
hyejin <fishhj1026@gmail.com>
2026-01-26 09:06:30 +0900

    geemini 브랜치에 commit

--- backend/src/main/java/com/jinie/todoList/controller/GeminiController.java
+++ backend/src/main/java/com/jinie/todoList/controller/GeminiController.java
@@ -26,7 +26,10 @@ public class GeminiController {
         log.debug("제미나이 요청");
         String prompt ="";
         prompt += "이게 내가 todolist 완료한 목록인데, 추천목록을 작성해줘";
-        prompt += "제미나이ai - spring ai 연동하기";
+        prompt += "/ 앞 단어는 카테고리이야";
+        prompt += "공부/1.제미나이ai - spring ai 연동하기";
+        prompt += "취미/1.수영 다녀오기";
+        prompt += "카테고리별 리스트형태로만 2개씩만 작성해서 알려줘";
 
        return geminiService.getGeminiReponse(prompt);
     }
